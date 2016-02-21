53344de91dcc81533cf58b19b0c71e50854bdd2b
Choirunnisa Fatima <choirunnisa.fatima@gmail.com>
2016-02-22 06:07:19 +0700

    add dateCreate to get all orders for apotek

--- src/main/java/com/catalina/tokobat/dto/ListTransactionApotekDto.java
+++ src/main/java/com/catalina/tokobat/dto/ListTransactionApotekDto.java
@@ -3,6 +3,7 @@ package com.catalina.tokobat.dto;
 import com.catalina.tokobat.entity.Transaction;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -19,7 +20,8 @@ public class ListTransactionApotekDto implements Serializable {
             TransactionSummary summary = new TransactionSummary(
                     trans.getId(),
                     trans.getUser().getName(),
-                    trans.isReadBy()
+                    trans.isReadBy(),
+                    trans.getDateCreate()
             );
             transactions.add(summary);
         }
@@ -30,12 +32,14 @@ public class ListTransactionApotekDto implements Serializable {
         public long transId;
         public String senderName;
         public boolean read;
+        public String dateCreate;
         
         public TransactionSummary(long transId, String senderName,
-                boolean read) {
+                boolean read, Calendar dateCreate) {
             this.transId = transId;
             this.senderName = senderName;
             this.read = read;
+            this.dateCreate = dateCreate.;
         }
     }
 }
